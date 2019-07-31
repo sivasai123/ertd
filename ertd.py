9133ad453cb4ce7610d7ed8ca7885cc5f53c39c5
sivasai123 <52394434+sivasai123@users.noreply.github.com>
2019-07-31 12:19:37 +0530

    Create ertd.py

---
+++ ertd.py
@@ -0,0 +1,10 @@
+ while (h > l) 
+    { 
+        if (str[l++] != str[h--]) 
+        { 
+            printf("%s is Not Palindrome", str); 
+            return; 
+        } 
+    } 
+    printf("%s is palindrome", str); 
+} 
